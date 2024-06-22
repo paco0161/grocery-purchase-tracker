bd522453f93573935b59efa6295eef698d48e228
paco0161 <paco0161@gmail.com>
2024-06-21 23:18:18 -0400

    declare package json a module

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: true,
     images: { domains: ['res.cloudinary.com'] },
-    transpilePackages: ['axios'],
+    // transpilePackages: ['axios'],
     rewrites: async () => {
         return [
         {
