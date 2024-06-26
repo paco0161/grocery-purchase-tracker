735b9ecc7e403cba6630d36128b75f76259fad4b
paco0161 <paco0161@gmail.com>
2024-06-25 23:40:04 -0400

    Exclude SWC package to try solve limited storage for serverless function

--- next.config.js
+++ next.config.js
@@ -14,6 +14,15 @@ const nextConfig = {
         },
         ]
     },
+    experimental: {
+        outputFileTracingExcludes: {
+          '*': [
+            'node_modules/@swc/core-linux-x64-gnu',
+            'node_modules/@swc/core-linux-x64-musl',
+            'node_modules/@esbuild/linux-x64',
+          ],
+        },
+    },
 }
 
 module.exports = nextConfig
